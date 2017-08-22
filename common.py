f32cc63595c604f01dd0e522d61f46285335fd8b
Bence Lovas <bansoul1@gmail.com>
2017-08-22 16:50:57 +0200

    elapsed time in seconds as well, logo center

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 
+
 def query_to_dict(data, label_list):
     data_dict = {}
     for item in data:
@@ -27,23 +28,23 @@ def elapsed_time(past_date):
 
 
 def td_format(td_object):
-        seconds = int(td_object.total_seconds())
-        periods = [
-                ('year',        60*60*24*365),
-                ('month',       60*60*24*30),
-                ('day',         60*60*24),
-                ('hour',        60*60),
-                # ('minute',      60),
-                # ('second',      1)
-                ]
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ('year',        60 * 60 * 24 * 365),
+        ('month',       60 * 60 * 24 * 30),
+        ('day',         60 * 60 * 24),
+        ('hour',        60 * 60),
+        ('minute',      60),
+        ('second',      1)
+    ]
 
-        strings=[]
-        for period_name,period_seconds in periods:
-                if seconds > period_seconds:
-                        period_value , seconds = divmod(seconds,period_seconds)
-                        if period_value == 1:
-                                strings.append("%s %s" % (period_value, period_name))
-                        else:
-                                strings.append("%s %ss" % (period_value, period_name))
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds > period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if period_value == 1:
+                strings.append("%s %s" % (period_value, period_name))
+            else:
+                strings.append("%s %ss" % (period_value, period_name))
 
-        return ", ".join(strings) + " AGO"
+    return ", ".join(strings) + " AGO"
